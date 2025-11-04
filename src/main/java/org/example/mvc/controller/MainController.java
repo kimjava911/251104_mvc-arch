10ad402a50436ccc9fc1655fc58fdd389ce991fa
kimjava911 <aibackend4th@gmail.com>
2025-11-04 14:35:54 +0900

    step05 + main controller

---
+++ src/main/java/org/example/mvc/controller/MainController.java
@@ -0,0 +1,20 @@
+package org.example.mvc.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.mvc.service.PetService;
+import org.example.mvc.service.PetServiceImpl;
+
+import java.io.IOException;
+
+@WebServlet("/")
+public class MainController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.sendRedirect(req.getContextPath() + "/pet");
+    }
+}
